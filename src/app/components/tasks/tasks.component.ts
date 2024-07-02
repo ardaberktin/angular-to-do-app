9a1544c934069c1e03f6c09a46196758e69692a7
Arda Berktin <arberktin@gmail.com>
2024-07-02 08:54:05 +0300

    upgrades

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../../../types';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [MatCheckboxModule, FormsModule],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss',
 })
 export class TasksComponent {
   @Input() task!: Task;
+  // TODO: Checkbox not working
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,25 +2,50 @@ import { Component } from '@angular/core';
 import { TasksComponent } from '../components/tasks/tasks.component';
 import { CommonModule } from '@angular/common';
 import { Task, Tasks } from '../../types';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TasksComponent, CommonModule],
+  imports: [TasksComponent, CommonModule, MatCheckboxModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   message: string = 'Widgets';
   totalTaskNum: number = 10;
-  tasks: Task[] = [{ name: '', body: '' }];
+  tasks: Task[] = [
+    { name: '', body: '' },
+    { name: '', body: '' },
+    { name: '', body: '' },
+    { name: '', body: '' },
+    { name: '', body: '' },
+  ];
 
   getTasks() {
-    this.tasks[0].name = 'deneme';
+    this.tasks[0].name = 'Deneme';
     this.tasks[0].body = 'deneme124325412541';
+
+    this.tasks[1].name = 'Example Task 1';
+    this.tasks[1].body = 'Description for Example Task 1';
+
+    this.tasks[2].name = 'Example Task 2';
+    this.tasks[2].body = 'Description for Example Task 2';
+
+    this.tasks[3].name = 'Example Task 3';
+    this.tasks[3].body = 'Description for Example Task 3';
+
+    this.tasks[4].name = 'Example Task 4';
+    this.tasks[4].body = 'Description for Example Task 4';
+  }
+
+  getTotalTasks() {
+    this.totalTaskNum = this.tasks.length;
   }
 
   ngOnInit(): void {
     this.getTasks();
+    this.getTotalTasks();
   }
 }
