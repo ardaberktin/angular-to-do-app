96e217cc8d7491692c7d4dacab0b679e8e41b9c3
Arda Berktin <arberktin@gmail.com>
2024-07-11 09:21:10 +0300

    added a underline in navbar depending on which page the user is on

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { HomeComponent } from '../../home/home.component';
 import { TasksComponent } from '../../tasks/tasks.component';
 import { ProfileComponent } from '../../profile/profile.component';
@@ -20,5 +22,22 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  public routerLinkVariable = '/home';
+  // public routerLinkVariable = '/home';
+  public currentRoute: string = '';
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.router.events
+      .pipe(
+        filter(
+          (event): event is NavigationEnd => event instanceof NavigationEnd
+        )
+      )
+      .subscribe((event: NavigationEnd) => {
+        this.currentRoute = event.urlAfterRedirects;
+      });
+  }
+
+  isActive(url: string): boolean {
+    return this.currentRoute === url;
+  }
 }
